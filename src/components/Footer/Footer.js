affc74cfbdbda0cd58d72b2bd6f6f18c48e0b545
Vladislav Grents <61117355+grentsva@users.noreply.github.com>
2022-03-11 00:38:55 +0500

    add Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import { FooterWrapper } from './FooterStyles';
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <span>Links</span>
+      <span>Sign</span>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/FooterStyles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const FooterWrapper = styled.div`
+  background-color: #181818;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 30px;
+  width: 100%;
+`;
