52353ac04c48b3e90445100d43d09e3494160e83
Vladislav Grents <61117355+grentsva@users.noreply.github.com>
2022-03-11 09:38:27 +0500

    add footerLinks and footerSign

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,16 @@
-import { FooterWrapper } from './FooterStyles';
+import { FooterWrapper, FooterSign, FooterLinks } from './FooterStyles';
 
 const Footer = () => {
   return (
     <FooterWrapper>
-      <span>Links</span>
-      <span>Sign</span>
+      <FooterLinks
+        href='https://github.com/grentsva'
+        rel='noreferrer'
+        target={'_blank'}
+      >
+        <i className='fab fa-github'></i>
+      </FooterLinks>
+      <FooterSign>Developed by Vladislav Grents, 2022</FooterSign>
     </FooterWrapper>
   );
 };
--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -7,4 +7,23 @@ export const FooterWrapper = styled.div`
   bottom: 0;
   height: 30px;
   width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const FooterLinks = styled.a`
+  color: #fff;
+  padding: 5px 10px;
+  text-decoration: none;
+
+  &:hover {
+    color: #346b21;
+    transition: 200ms ease-in;
+  }
+`;
+
+export const FooterSign = styled.div`
+  flex-basis: 50%;
+  padding: 5px 10px;
 `;
